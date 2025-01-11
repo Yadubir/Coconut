cb3d3accb61bf68a3c88ca91c6d285921a780a04
Yadubir <yadubirjha2022@vitbhopal.ac.in>
2025-01-11 21:34:46 +0530

    Added the route for saving problemsId to a particular user database

--- Backend/Middlewares/authMiddleware.js
+++ Backend/Middlewares/authMiddleware.js
@@ -2,30 +2,41 @@ const jwt = require("jsonwebtoken");
 const User = require("../Models/User");
 
 const protect = async (req, res, next) => {
-    try {
-      const token = req.cookies.token;
-      if(!token) {
-        return res.status(401).json({
-          success: false,
-          message: "Not authorized, no token"
-        });
-      }
-
-      const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-
-      if (!decoded) {
-        return res.status(401).json({
-          success: false,
-          message: "Not authorized, token failed",
-        });
-      }
-
-      req.user = await User.findById(decoded.id).select("-password");
-
-      next();
-    } catch (error) {
-      res.status(401).json({ message: "Not authorized, token failed" });
+  try {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Not authorized, no token",
+      });
     }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({
+        success: false,
+        message: "Not authorized, token failed",
+      });
+    }
+
+    // console.log(decoded.userId);
+
+    req.user = await User.findById(decoded.userId).select("-password");
+    console.log(req.user);
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Not authorized, token failed",
+    });
+  }
 };
 
 module.exports = { protect };
